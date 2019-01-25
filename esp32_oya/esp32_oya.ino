1425818e8a58f1a53d2fac24d9481902ed767e6c
高倉礼 <reioffitial@gmail.com>
2019-01-25 17:57:56 +0900

    lightingに分離。

--- esp32_oya/esp32_oya.ino
+++ esp32_oya/esp32_oya.ino
@@ -58,47 +58,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
   }
 };
 
-float chaos(float seed) {
-  if(seed < 0.5){
-    seed = seed + 2*seed*seed;
-  } else {
-    seed = seed - 2*(1.0-seed)*(1.0-seed);
-  }
-  if(seed < 0.05) seed = 0.05;
-  if(seed > 0.95) seed = 0.95;
-  return seed;
-}
 
-void chaosBlink() {
-  seed = chaos(seed); // seed値の更新
-//  Serial.println("Seed value:" + String(seed) );
-  
-  for(uint16_t i=0; i<255; i++){
-    c = strip.Color(i, i, i);
-    for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-    }
-    delay(1);
-    if( touchCallback() ) return;
-    if( readCallback() ) return;
-    strip.show();
-    delay(30*seed);
-  }
-  for(uint16_t i=255; i>0; i--){
-    c = strip.Color(i, i, i);
-    for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-    }
-    delay(1);
-    if( touchCallback() ) return;
-    if( readCallback() ) return;
-    strip.show();
-    delay(30*seed);
-  }
-  
-  delay(1);
-  strip.show(); 
-}
 
 bool touchCallback() {
   int in = analogRead(33);
@@ -147,31 +107,6 @@ bool readCallback() {
     }
   }
   return false;
-}
-
-
-// タッチされた時の光り方を制御している関数
-void touchLighting() {
-  for(int j=0; j<3; j++){
-    for(uint16_t i=0; i<255; i++){
-      c = strip.Color(i, i, i);
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, c);
-      }
-      delay(1);
-      strip.show();
-      delay(0.01);
-    }
-    for(uint16_t i=255; i>0; i--){
-      c = strip.Color(i, i, i);
-      for(uint16_t i=0; i<strip.numPixels(); i++) {
-        strip.setPixelColor(i, c);
-      }
-      delay(1);
-      strip.show();
-      delay(0.01);
-    }
-  }
 }
 
 
